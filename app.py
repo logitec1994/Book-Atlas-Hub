1f3ae46c9e06316bd713ae6ad1931e651ee696b3
logitec1994 <123654978a@gmail.com>
2023-12-18 01:09:52 +0200

    Added login endpoint and modified registration endpoint

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask
+from api.routes import api_bp
+from api.routes import db, bcrypt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some_perfect_secret_key'
 
+app.register_blueprint(api_bp)
 
-@app.route("/api/auth/register")
-def registration():
-    return "Hello registration"
+db.init_app(app)
+bcrypt.init_app(app)
 
 
 if __name__ == "__main__":
